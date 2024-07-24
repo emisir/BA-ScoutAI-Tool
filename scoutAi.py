1d469beff3eec147dd64e985a1e3d2da0bebd71c
emisir <ebubekir.misir@studmail.hs-aalen.de>
2024-07-24 13:36:03 +0200

    adjustments

--- scoutAi.py
+++ scoutAi.py
@@ -78,7 +78,7 @@ pandas_prompt = PromptTemplate(pandas_prompt_str).partial_format(
 )
 pandas_output_parser = PandasInstructionParser(df)
 response_synthesis_prompt = PromptTemplate(response_synthesis_prompt_str)
-llm = OpenAI(model="gpt-4-turbo")
+llm = OpenAI(api_key=api_key, model="gpt-4-turbo")  
 
 # Erstellen der QueryPipeline mit den Modulen
 qp = QP(
